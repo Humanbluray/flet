39c05b1b4f07b98d6b072cf4ee7a185dc35052cb
Humanbluray <vancarlosfr@gmail.com>
2024-10-25 02:14:06 +0100

    first commit

--- Complete Todo/main.py
+++ Complete Todo/main.py
@@ -1,45 +1,45 @@
-from flet import *
+import flet as ft
 
-_dark: str = colors.with_opacity(0.5, "white")
-_light: str = colors.with_opacity(1, 'black')
+_dark: str = ft.colors.with_opacity(0.5, "white")
+_light: str = ft.colors.with_opacity(1, 'black')
 todo_item_style_sheet: dict = {"height": 50, "border_radius": 4}
 
 
-class TodoItem(Container):
+class TodoItem(ft.Container):
     def __init__(self, hero: object, description: str, theme: str)->None:
 
         if theme == 'dark':
-            todo_item_style_sheet['border'] = border.all(1, _dark)
+            todo_item_style_sheet['border'] = ft.border.all(1, _dark)
         else:
-            todo_item_style_sheet['border'] = border.all(1, _light)
+            todo_item_style_sheet['border'] = ft.border.all(1, _light)
 
         super().__init__(**todo_item_style_sheet)
         self.hero = hero
         self.description = description
 
-        self.tick = Checkbox(on_change=self.strike)
-        self.text = Text(spans=[TextSpan(text=self.description)], size=14)
-        self.delete = IconButton(
-            icon=icons.DELETE_OUTLINE,
-            icon_color=colors.RED_700,
+        self.tick = ft.Checkbox(on_change=self.strike)
+        self.text = ft.Text(spans=[ft.TextSpan(text=self.description)], size=14)
+        self.delete = ft.IconButton(
+            icon=ft.icons.DELETE_OUTLINE,
+            icon_color=ft.colors.RED_700,
             on_click=self.delete_item
         )
 
-        self.content = Row(
-            alignment=MainAxisAlignment.SPACE_BETWEEN,
+        self.content = ft.Row(
+            alignment=ft.MainAxisAlignment.SPACE_BETWEEN,
             controls=[
-                Row(controls=[self.tick, self.text]),
+                ft.Row(controls=[self.tick, self.text]),
                 self.delete
             ]
         )
 
     def strike(self, e):
         if self.tick.value is True:
-            self.text.spans[0].style = TextStyle(
-                decoration=TextDecoration.LINE_THROUGH, decoration_thickness=2
+            self.text.spans[0].style = ft.TextStyle(
+                decoration= ft.TextDecoration.LINE_THROUGH, decoration_thickness=2
             )
         else:
-            self.text.spans[0].style = TextStyle()
+            self.text.spans[0].style =  ft.TextStyle()
 
         self.text.update()
 
@@ -49,59 +49,57 @@ class TodoItem(Container):
         self.hero.todo_area.update()
 
 
-class Hero(SafeArea):
-    def __init__(self, page: Page):
-        super().__init__(minimum=10, maintain_bottom_view_padding=True)
+class Hero(ft.Container):
+    def __init__(self, page:  ft.Page):
+        super().__init__()
         self.page = page
 
-        self.title = Text("Todo List", size=20, weight=FontWeight.W_800)
-        self.toggle = IconButton(icon_size=18, icon=icons.DARK_MODE_ROUNDED, on_click=self.switch)
-        self.item = TextField(height=50, expand=True, border_color=_dark, cursor_height=24,
+        self.title =  ft.Text("Todo List", size=20, weight= ft.FontWeight.W_800)
+        self.toggle =  ft.IconButton(icon_size=18, icon= ft.icons.DARK_MODE_ROUNDED, on_click=self.switch)
+        self.item =  ft.TextField(height=50, expand=True, border_color=_dark, cursor_height=24,
                               hint_text="Add you todo item here...", content_padding=15)
-        self.add = IconButton(icon=icons.ADD_ROUNDED, icon_size=18, on_click=self.add_item)
+        self.add =  ft.IconButton(icon= ft.icons.ADD_ROUNDED, icon_size=18, on_click=self.add_item)
 
-        self.todo_area = Column(expand=True, spacing=18)
-        self.counter = Text("0 items", italic=True)
-
-        self.main = Column(
+        self.todo_area =  ft.Column(expand=True, spacing=18)
+        self.counter =  ft.Text("0 items", italic=True)
+        self.content = ft.Column(
             controls=[
-                Row(
-                    alignment=MainAxisAlignment.SPACE_BETWEEN,
+                ft. Row(
+                    alignment= ft.MainAxisAlignment.SPACE_BETWEEN,
                     controls=[self.title, self.toggle],
                 ),
-                Divider(height=20),
-                Divider(height=20, color="transparent"),
-                Text("Add your item below"),
-                Row(
-                    alignment=MainAxisAlignment.SPACE_BETWEEN,
+                ft.Divider(height=20),
+                ft.Divider(height=20, color="transparent"),
+                ft.Text("Add your item below"),
+                ft.Row(
+                    alignment=ft.MainAxisAlignment.SPACE_BETWEEN,
                     controls=[self.item, self.add]
                 ),
-                Divider(height=20, color="transparent"),
-                Row(
-                    alignment=MainAxisAlignment.SPACE_BETWEEN,
-                    controls=[Text("2. list of todo items"), self.counter]
+                ft.Divider(height=20, color="transparent"),
+                ft.Row(
+                    alignment=ft.MainAxisAlignment.SPACE_BETWEEN,
+                    controls=[ft.Text("2. list of todo items"), self.counter]
                 ),
                 self.todo_area
             ]
         )
-        self.content = self.main
 
     def switch(self, e):
         if self.page.theme_mode == 'dark':
             self.page.theme_mode = 'light'
-            self.toggle.icon = icons.LIGHT_MODE_ROUNDED
+            self.toggle.icon = ft.icons.LIGHT_MODE_ROUNDED
             self.item.border_color = _light
 
             for item in self.todo_area.controls[:]:
-                item.border = border.all(1, _light)
+                item.border = ft.border.all(1, _light)
 
         else:
             self.page.theme_mode = 'dark'
-            self.toggle.icon = icons.DARK_MODE_ROUNDED
+            self.toggle.icon = ft.icons.DARK_MODE_ROUNDED
             self.item.border_color = _dark
 
             for item in self.todo_area.controls[:]:
-                item.border = border.all(1, _dark)
+                item.border = ft.border.all(1, _dark)
 
         # self.todo_area.update()
         self.page.update()
@@ -129,9 +127,9 @@ class Hero(SafeArea):
             pass
 
 
-def main(page: Page):
-    page.theme_mode ='dark'
-    theme = Theme()
+def main(page: ft.Page):
+    page.theme_mode = ft.ThemeMode.DARK
+    theme = ft.Theme()
     page.theme = theme
 
     hero = Hero(page)
@@ -139,5 +137,6 @@ def main(page: Page):
     page.update()
 
 
+
 if __name__ == '__main__':
-    app(target=main)
+    ft.app(target=main)
